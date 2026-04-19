5d76e683bf3d60541d579e54ea19ab3df94770d2
Scarletra <fernandovalentino10@gmail.com>
2026-04-19 23:57:16 +0700

    feat: update test to fulfill the case req and add google auth to bypass 2fa

--- src/test/java/com/scarletra/GmailTest.java
+++ src/test/java/com/scarletra/GmailTest.java
@@ -1,6 +1,7 @@
 package com.scarletra;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,6 +15,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 
 public class GmailTest {
@@ -23,14 +25,14 @@ public class GmailTest {
     // https://www.icertglobal.com/community/automate-gmail-login-with-selenium-webdriver-java
     // Use WebDriverWait to mimic human behavior and avoid detection by Google as a bot (scripted)
 
-
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
         
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled");
-        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
         options.addArguments("--remote-allow-origins=*");
         
         driver = new ChromeDriver(options);
@@ -51,26 +53,38 @@ public class GmailTest {
             passInput.sendKeys("Scarletra05");
             driver.findElement(By.id("passwordNext")).click();
 
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("tr.zE")));
-            List<WebElement> unreadEmails = driver.findElements(By.cssSelector("tr.zE"));
+            String secretKey = "xq4ntt7ifysxfwiajgy65hjzbkmzz2pg"; 
+            GoogleAuthenticator gAuth = new GoogleAuthenticator();
+            String otpCode = String.valueOf(gAuth.getTotpPassword(secretKey));
 
-            int count = Math.min(5, unreadEmails.size());
+            WebElement otpInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("totpPin")));
+            otpInput.sendKeys(otpCode);
+            driver.findElement(By.id("totpNext")).click();
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("tr.zE")));
+            List<WebElement> unreadEmails = driver.findElements(By.cssSelector("tr.zE"));
+            
+            Assert.assertTrue(unreadEmails.size() > 0, "No unread emails found.");
+
+            WebElement lastUnread = unreadEmails.get(0);
+            String subject = lastUnread.findElement(By.cssSelector("span.bog")).getText();
             
             System.out.println("----------------------------------------------");
-            System.out.println("LOG: Showing " + count + " last unread emails:");
-            
-            for (int i = 0; i < count; i++) {
-                WebElement emailRow = unreadEmails.get(i);
-                String subject = emailRow.findElement(By.cssSelector("span.bog")).getText();
-                System.out.println((i + 1) + ". Subject: " + subject);
-            }
-            
+            System.out.println("LOG: Last Unread Email Title: " + subject);
             System.out.println("----------------------------------------------");
-            
-            Assert.assertTrue(unreadEmails.size() >= 5, "Email unread is less than 5.");
+
+            WebElement checkbox = lastUnread.findElement(By.cssSelector("div[role='checkbox']"));
+            checkbox.click();
+
+            WebElement deleteButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[aria-label='Delete']")));
+            deleteButton.click();
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), 'moved to Bin')]")));
+            System.out.println("LOG: Successfully deleted email: " + subject);
 
         } catch (Exception e) {
             e.printStackTrace();
+            Assert.fail("Test failed due to: " + e.getMessage());
         }
     }
 
