ef92982fbcf42779c7074f5350dc42d3b2d6a7d0
Scarletra <fernandovalentino10@gmail.com>
2026-04-19 19:29:53 +0700

    feat: update gmail test script and use web driver wait lib

--- src/test/java/com/scarletra/GmailTest.java
+++ src/test/java/com/scarletra/GmailTest.java
@@ -6,47 +6,71 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 import java.util.List;
 
 public class GmailTest {
     WebDriver driver;
+    WebDriverWait wait;
+
+    // https://www.icertglobal.com/community/automate-gmail-login-with-selenium-webdriver-java
+    // Use WebDriverWait to mimic human behavior and avoid detection by Google as a bot (scripted)
+
 
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
-
+        
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.addArguments("--remote-allow-origins=*");
         
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         driver.manage().window().maximize();
     }
 
     @Test
-    public void testGmailDeletion() throws InterruptedException {
-        driver.get("https://mail.google.com/mail/");
+    public void testGmailScenario() {
+        try {
+            driver.get("https://mail.google.com/mail/");
 
-        driver.findElement(By.id("identifierId")).sendKeys("anonym100205@gmail.com");
-        driver.findElement(By.id("identifierNext")).click();
+            WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("identifierId")));
+            emailInput.sendKeys("anonym100205@gmail.com");
+            driver.findElement(By.id("identifierNext")).click();
 
-        Thread.sleep(3000); 
+            WebElement passInput = wait.until(ExpectedConditions.elementToBeClickable(By.name("Passwd")));
+            passInput.sendKeys("Scarletra05");
+            driver.findElement(By.id("passwordNext")).click();
 
-        driver.findElement(By.name("Passwd")).sendKeys("Scarletra05");
-        driver.findElement(By.id("passwordNext")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("tr.zE")));
+            List<WebElement> unreadEmails = driver.findElements(By.cssSelector("tr.zE"));
 
-        List<WebElement> unreadEmails = driver.findElements(By.cssSelector("tr.zE"));
+            int count = Math.min(5, unreadEmails.size());
+            
+            System.out.println("----------------------------------------------");
+            System.out.println("LOG: Showing " + count + " last unread emails:");
+            
+            for (int i = 0; i < count; i++) {
+                WebElement emailRow = unreadEmails.get(i);
+                String subject = emailRow.findElement(By.cssSelector("span.bog")).getText();
+                System.out.println((i + 1) + ". Subject: " + subject);
+            }
+            
+            System.out.println("----------------------------------------------");
+            
+            Assert.assertTrue(unreadEmails.size() >= 5, "Email unread is less than 5.");
 
-        if (!unreadEmails.isEmpty()) {
-            WebElement lastUnread = unreadEmails.get(0);
-            String subject = lastUnread.findElement(By.cssSelector("span.bog")).getText();
-            System.out.println("LOG: Title email unread terakhir adalah -> " + subject);
-        } else {
-            System.out.println("LOG: Tidak ada email unread.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
